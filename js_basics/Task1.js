cbbf017ff8a4baa52235a75a7b66df409f1ffa1f
Santiago <alexsa.beauty@gmail.com>
2023-10-11 23:04:30 +0300

    Completed tasks using JavaScript (5 tasks)

---
+++ js_basics/Task1.js
@@ -0,0 +1,11 @@
+let catWeight = 6
+const CAT_NAME = 'Night'
+let isHealthy = true
+let catPassportNumber = null
+let catBreed
+
+console.log('Cat weight = '+catWeight+' kg')
+console.log('Cat name is "'+CAT_NAME+'"')
+console.log('Is the cat healthy? '+isHealthy)
+console.log('Passport number: '+catPassportNumber)
+console.log('Cat breed is "'+catBreed+'"')
---
+++ js_basics/Task4.js
@@ -0,0 +1,4 @@
+let heroAge = 25
+let isAdult = heroAge>=18
+
+console.log('Is the Hero adult? '+isAdult+'. His age is '+heroAge+' years old')
