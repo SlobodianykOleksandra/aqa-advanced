4238e0d372802dc9ae7d4420896908fc6e0f0049
Santiago <alexsa.beauty@gmail.com>
2023-10-31 18:56:49 +0200

    Added return values to the function

--- arrays/Task1.js
+++ arrays/Task1.js
@@ -1,13 +1,10 @@
 const numbers1 = [2, -5, 0, 7, -3, 0, 10, -8, 'Hello', -8, 5]
 const numbers2 = [5, 9, 0 ,-10, 7, 0, -1, -99]
 
-let positiveCount
-let negativeCount
-let zeroCount
 function countValues (array){
-    positiveCount = 0
-    negativeCount = 0
-    zeroCount = 0
+    let positiveCount = 0
+    let negativeCount = 0
+    let zeroCount = 0
     for (let i = 0;i<array.length;i++){
         switch (true){
             case array[i] === 0:
@@ -23,15 +20,17 @@ function countValues (array){
                 console.log(`Value ${array[i]} on index ${i} is NaN`)
         }
     }
+    return [positiveCount, negativeCount, zeroCount]
 }
-countValues(numbers1)
-console.log('Count of positive numbers: ',positiveCount)
-console.log('Count of negative numbers: ',negativeCount)
-console.log('Count of zero: ',zeroCount)
+
+const count1 = countValues(numbers1)
+console.log('Count of positive numbers: ',count1[0])
+console.log('Count of negative numbers: ',count1[1])
+console.log('Count of zero: ',count1[2])
 
 console.log('---------------------')
 
-countValues(numbers2)
-console.log('Count of positive numbers: ',positiveCount)
-console.log('Count of negative numbers: ',negativeCount)
-console.log('Count of zero: ',zeroCount)
+const count2 = countValues(numbers2)
+console.log('Count of positive numbers: ',count2[0])
+console.log('Count of negative numbers: ',count2[1])
+console.log('Count of zero: ',count2[2])
