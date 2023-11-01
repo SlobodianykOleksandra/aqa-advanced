b1d317ceb2e627d06afb1d4b4adb2c364aa6a8e9
Santiago <alexsa.beauty@gmail.com>
2023-11-02 00:18:27 +0200

    Completed homework about Objects

---
+++ objects/Task2.js
@@ -0,0 +1,6 @@
+const book = {
+    title: 'The Witcher: The Last Wish',
+    author: 'Andrzej Sapkowski',
+    year: 1993
+}
+console.log(`${book.title} is a short story collection written by ${book.author}`)
